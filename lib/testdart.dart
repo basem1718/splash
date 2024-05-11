4ad7f1b690788661ae84452f91e23e689d5cfc68
Basem Mustafa <bbaasseemm666@gmail.com>
2024-05-11 17:14:13 +0300

    uploaded

---
+++ lib/testdart.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class TestDart extends StatelessWidget {
+  const TestDart({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Row(
+        children: [Text("data")],
+      ),
+    );
+  }
+}
